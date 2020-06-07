2e49c8ee22d509ed095bc7437c9f90c782748624
derkallevombau <kalle@Aspire2>
2020-06-07 16:52:59 +0200

    typedoc.js:
    Set "out" to GitHub Pages dir.
    
    Set "entryPoint" (see comment).

--- typedoc.js
+++ typedoc.js
@@ -1,15 +1,13 @@
 module.exports =
 {
 	mode: 'modules',
-	toc: [
-		'FhemClient'
-	],
-	out: 'doc',
+	out: 'docs', // GitHub Pages dir
 	listInvalidSymbolLinks: true,
 	excludePrivate: true,
 	excludeProtected: true,
-	stripInternal: true,
+	stripInternal: true, // See tsconfig.json.
 	disableSources: true,
 	readme: 'none',
-	includeVersion: true
+	includeVersion: true,
+	entryPoint: '"fhem-client"' // No modules overview since we have just one.
 };
